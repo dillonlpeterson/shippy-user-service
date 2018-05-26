3abda66c7270b1f5985150f302014d263f5a8432
Dillon L. Peterson <dillonpeterson@mac.com>
2018-05-26 15:55:09 -0500

    Maybe

--- proto/user/extensions.go
+++ proto/user/extensions.go
@@ -8,9 +8,9 @@ import (
 // BeforeCreate Hooks into GORMS event lifecycle (Called before new entity is saved).
 // Notice, we don't need to manually manage database connections here.
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	randUUID := uuid.Must(uuid.NewV4())
-	/*if err != nil {
+	randUUID, err := uuid.NewV4()
+	if err != nil {
 		return err
-	}*/
+	}
 	return scope.SetColumn("Id", randUUID.String())
 }
