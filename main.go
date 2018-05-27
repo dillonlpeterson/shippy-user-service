06fc55024532038ccb8bba4270f55153ce925fc6
Dillon L. Peterson <dillonpeterson@mac.com>
2018-05-27 12:39:42 -0500

    If at first you dont succeed, try try again

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	// Create a new service
 	srv := micro.NewService(
 		// This name must match the package name given in the protobuf definition
-		micro.Name("shippy.auth"),
+		micro.Name("shippy.user"),
 		micro.Version("latest"),
 	)
 
